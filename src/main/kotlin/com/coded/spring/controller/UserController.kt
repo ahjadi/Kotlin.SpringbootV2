c6af91e0ffa5c7e3886894bfa243dbf14d60e92e
ahjadi <alihussainaljadi@gmail.com>
2025-04-17 18:17:46 +0300

    BONUS Did password validation for Online Ordering - User Authentication

--- src/main/kotlin/com/coded/spring/controller/UserController.kt
+++ src/main/kotlin/com/coded/spring/controller/UserController.kt
@@ -2,13 +2,15 @@ package com.coded.spring.controller
 
 import com.coded.spring.entity.UserEntity
 import com.coded.spring.service.UserService
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class UserController(private val userService: UserService) {
+class UserController(private val userService: UserService,
+    private val encoder: PasswordEncoder) {
 
     @GetMapping("/hello")
     fun hello()= """
@@ -21,12 +23,20 @@ class UserController(private val userService: UserService) {
     """.trimIndent()
 
     @PostMapping("/public/users/create")
-    fun newUser(@RequestBody user: UserEntity): UserEntity {
-        return userService.createUser(user)
+    fun newUser(@RequestBody userRequest: UserRequest): Any {
+        userService.validatePassword(userRequest.password)
+        return userService.createUser(UserEntity(name = userRequest.name, email = userRequest.email, username = userRequest.username, password = encoder.encode(userRequest.password) ))
     }
 
     @GetMapping("/users/v1/list")
     fun listUsers() = userService.findAllUsers()
 }
+
+data class UserRequest(
+    val name: String,
+    val email: String,
+    val username: String,
+    val password: String
+)
 
 
--- src/main/kotlin/com/coded/spring/service/UserService.kt
+++ src/main/kotlin/com/coded/spring/service/UserService.kt
@@ -12,5 +12,10 @@ class UserService (private val userRepository: UserRepository){
 
     fun createUser(user: UserEntity) = userRepository.save(user)
 
+    fun validatePassword(password: String) {
+        require(password.length >= 6) { "Password must be at least 6 characters long" }
+        require(password.any { it.isUpperCase() }) { "Password must contain at least one uppercase letter" }
+        require(password.any { it.isDigit() }) { "Password must contain at least one number" }
+    }
 
 }
